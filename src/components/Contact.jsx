f6fe9b9c4050d6463fa05a6bcd4f15c36f72f879
mranv <iamanubhavgain@gmail.com>
2025-05-18 13:24:14 +0530

    feat: Add alt attributes to images in Contact component for improved accessibility

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,25 @@
 import AnimatedTitle from "./AnimatedTitle";
 import Button from "./Button";
 
-const ImageClipBox = ({ src, clipClass }) => (
+const ImageClipBox = ({ src, clipClass, alt }) => (
   <div className={clipClass}>
-    <img src={src} />
+    <img src={src} alt={alt} />
   </div>
 );
 
 const Contact = () => {
   return (
-    <div id="contact" className="my-20 min-h-96 w-screen  px-10">
+    <div id="contact" className="my-20 min-h-96 w-screen px-10">
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
             src="/img/contact-1.webp"
+            alt="Cybersecurity illustration 1"
             clipClass="contact-clip-path-1"
           />
           <ImageClipBox
             src="/img/contact-2.webp"
+            alt="Cybersecurity illustration 2"
             clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
           />
         </div>
@@ -25,10 +27,12 @@ const Contact = () => {
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
           <ImageClipBox
             src="/img/swordman-partial.webp"
+            alt="Cybersecurity swordsman graphic - partial view"
             clipClass="absolute md:scale-125"
           />
           <ImageClipBox
             src="/img/swordman.webp"
+            alt="Cybersecurity swordsman full graphic"
             clipClass="sword-man-clip-path md:scale-125"
           />
         </div>
